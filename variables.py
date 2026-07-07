1512700f2c5b36d1937754054bbd5c5067a151fe
genie2510 <raveelucky@gmail.com>
2026-07-07 10:25:24 +0530

    Created to Master Python

---
+++ variables.py
@@ -0,0 +1,42 @@
+from traceback import print_tb
+
+name = "Ravi"
+age = 41
+height = 5.6
+is_student = True
+is_teacher = False
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(is_teacher)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(type(is_teacher))
+
+a = b = c =199
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+name, age, height, is_student = "Ravi Kumar", 42, 5.7, False
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+d,e = 50,60
+print(d)
+print(e)
+#swap
+d,e = e,d
+print(d)
+print(e)
+
+
