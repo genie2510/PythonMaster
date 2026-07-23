0e801bd06813122c2ec5204c55218f5d470f7fc3
genie2510 <raveelucky@gmail.com>
2026-07-23 09:07:01 +0530

    Tuples leverage

---
+++ workspace-py/com/leverage/tutorial/tuples.py
@@ -0,0 +1,12 @@
+#tuplename = (element1,element2,element3,....)
+
+integer_tuple = (1, 2, 3)
+print(integer_tuple)
+print(integer_tuple[1])
+print(type(integer_tuple))
+
+integerplus_tuple = (4,5,6)
+print(integerplus_tuple)
+
+total_tuple = integerplus_tuple + integerplus_tuple
+print(total_tuple)
--- workspace-py/com/leverage/tutorial/variables.py
+++ workspace-py/com/leverage/tutorial/variables.py
@@ -1,11 +1,14 @@
 from traceback import print_tb
-
+sig = 'TODOOOO'
 name = "Ravi"
 age = 41
 height = 5.6
 is_student = True
 is_teacher = False
 
+print(sig)
+sig=True # change datatype
+print(sig)
 print(name)
 print(age)
 print(height)
@@ -18,13 +21,15 @@ print(type(height))
 print(type(is_student))
 print(type(is_teacher))
 
-a = b = c =199
+a = b = c =199 #assign to multiple variables
 print(a)
 print(b)
 print(c)
 
 print(type(a))
 print(type(b))
+
+#define variables of different datatypes in a single line
 name, age, height, is_student = "Ravi Kumar", 42, 5.7, False
 print(name)
 print(age)
